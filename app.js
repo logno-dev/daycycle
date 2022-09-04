36727cb7b13e63a7e1d6184c0db7a0541ae4dd36
bunch <bunch@pop-os.localdomain>
2022-09-04 01:44:11 -0700

    added realtime mode

--- app.js
+++ app.js
@@ -9,9 +9,13 @@ const moon = document.getElementById("moon");
 const dayMountains = document.getElementById("day-mountains");
 const stars = document.querySelector(".stars");
 const cloud = document.querySelector(".cloud");
+const modeToggle = document.getElementById("mode_toggle");
+
+let sliderMode = true
 
 
 function setClock() {
+  if (sliderMode) {
     minute.innerText = String(((slider.value % 4) * 15)).padStart(2, '0');
     if (slider.value < 48){
         ampm.innerText = "AM";
@@ -25,16 +29,23 @@ function setClock() {
     } else {
         hour.innerText = String(Math.floor(slider.value / 4)).padStart(2, '0');
     }
+  } else {
+    let currentTime = new Date()
+    hour.innerText = currentTime.getHours() % 12 || 12;
+    minute.innerText = currentTime.getMinutes().toString().padStart(2, '0');
+    ampm.innerText = (currentTime.getHours() < 12) ? "AM" : "PM";
+  }
 
 }
 
+
 function setBackground() {
-    if (slider.value <= 48) {
-        daylight.style.backgroundColor = `rgb(99, 211, 242, ${(slider.value / 48)*1.5})`
-    } else {
-        daylight.style.backgroundColor = `rgb(99, 211, 242, ${((96 / slider.value)*2) - 2})`;
-    }
-    // console.log(daylight.style.backgroundColor)
+  if (slider.value <= 48) {
+      daylight.style.backgroundColor = `rgb(99, 211, 242, ${(slider.value / 48)*1.5})`
+  } else {
+      daylight.style.backgroundColor = `rgb(99, 211, 242, ${((96 / slider.value)*2) - 2})`;
+  }
+  // console.log(daylight.style.backgroundColor)
 }
 
 function moveSun() {
@@ -83,6 +94,92 @@ function handleSlider() {
     setCloud();
 }
 
+function changeEnvOnTick() {
+  let currentTime = new Date();
+  // let interval = 0.5;
+  let interval = (currentTime.getHours() + (currentTime.getMinutes()/60))/24;
+  if (interval < 0.2) {
+    daylight.style.backgroundColor = `rgb(99, 211, 242, 0)`;
+    stars.style.opacity = '1';
+    clock.style.color = 'white';
+    sun.style.bottom = `${(interval/0.5)*100-20}vh`;
+    moon.style.top = `${(interval/0.5)*100+10}vh`;
+    dayMountains.style.opacity = `${interval/0.5}`;
+    cloud.style.transform = 'translateX(-50vw)';
+  } else if (interval < 0.4) {
+    daylight.style.backgroundColor = `rgb(99, 211, 242, ${(interval-0.2)/0.2})`;
+    stars.style.opacity = `${1-(interval-0.2)/0.2}`;
+    clock.style.color = 'white';
+    sun.style.bottom = `${(interval/0.5)*100-20}vh`;
+    moon.style.top = `${(interval/0.5)*100+10}vh`;
+    dayMountains.style.opacity = `${interval/0.5}`;
+    cloud.style.transform = 'translateX(-50vw)';
+  } else if (interval >= 0.4 && interval <= 0.6) {
+    daylight.style.backgroundColor = "rgb(99, 211, 242, 1)";
+    stars.style.opacity = '0';
+    clock.style.color = 'black';
+    sun.style.bottom = `80vh`;
+    moon.style.top = `100vh`;
+    cloud.style.transform = 'translateX(0)';
+    if (interval <= 0.5) {
+      sun.style.bottom = `${(interval/0.5)*100-20}vh`;
+      moon.style.top = `${(interval/0.5)*100+20}vh`;
+      dayMountains.style.opacity = `${interval/0.5}`;
+    } else {
+      sun.style.bottom = `${((1-interval)/0.5)*100-20}vh`;
+      moon.style.top = `${((1-interval)/0.5)*100+10}vh`;
+      dayMountains.style.opacity = `${(1-interval)/0.5}`;
+    }
+  } else if (interval < 0.8) {
+    daylight.style.backgroundColor = `rgb(99, 211, 242, ${1-((interval-0.8)/0.2)})`;
+    stars.style.opacity = `${(interval-0.8)/0.8}`;
+    clock.style.color = 'white';
+    sun.style.bottom = `${((1-interval)/0.5)*100-20}vh`;
+    moon.style.top = `${((1-interval)/0.5)*100+10}vh`;
+    dayMountains.style.opacity = `${(1-interval)/0.5}`;
+    cloud.style.transform = 'translateX(-50vw)';
+  } else {
+    daylight.style.backgroundColor = `rgb(99, 211, 242, 0`;
+    stars.style.opacity = '1';
+    clock.style.color = 'white';
+    sun.style.bottom = `${((1-interval)/0.5)*100-20}vh`;
+    moon.style.top = `${((1-interval)/0.5)*100+10}vh`;
+    dayMountains.style.opacity = `${(1-interval)/0.5}`;
+    cloud.style.transform = 'translateX(-50vw)';
+  }
+}
+
 slider.addEventListener('mousedown', () => {
     slider.addEventListener('mousemove', handleSlider)
 });
+
+function handleInterval() {
+  if (!sliderMode) {
+    setClock();
+    changeEnvOnTick();
+  }
+}
+
+let clockInterval
+
+modeToggle.addEventListener('click', (event) => {
+  event.preventDefault();
+  if (sliderMode) {
+    modeToggle.innerText = "Switch to Slider";
+    slider.classList.add("hidden");
+    sliderMode = !sliderMode
+    handleInterval();
+    if (!clockInterval) {
+      clockInterval = setInterval(handleInterval, 1000)
+    }
+  } else {
+    modeToggle.innerText = "Switch to Realtime";
+    slider.classList.remove("hidden")
+    sliderMode = !sliderMode;
+    clearInterval(clockInterval)
+    clockInterval = null
+    handleSlider();
+  }
+
+})
+
